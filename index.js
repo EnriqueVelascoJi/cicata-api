d6f4b653966c42d8ef59c81dafcb9843b226438b
Luis Vazquez <luvazpa@gmail.com>
2021-01-03 19:06:26 -0600

    Initial config

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const { config } = require('./config');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(config.port, () => console.log(`Server on port ${config.port}`));
